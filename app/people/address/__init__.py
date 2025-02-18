c56930d3cfea024f2ae7a59d3560d125881dade0
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-02-18 14:38:08 -0500

    Add empty people and address apps.

---
+++ app/people/address/__init__.py
@@ -0,0 +1,1 @@
+# A person may have multiple addresses, such as "work", "home", "vacation home", etc.
---
+++ app/people/person/__init__.py
@@ -0,0 +1,1 @@
+# A person is a single person or company (the latter may become a separate app).
