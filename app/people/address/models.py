bbb27b6d27840603ad2f3ba3d3a18000a747e84f
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-02-26 16:29:21 -0500

    Create Address model.

--- app/people/address/models.py
+++ app/people/address/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    """An address of a person, organization, or place.
+
+    Currently US only.
+
+    Person, at least, will have a many-to-many relationship to Address. It should be defined in the Person model, as
+    that is considered the 'primary' model.
+    """
+
+    label = models.CharField(max_length=255)
+    first_line = models.CharField(max_length=255)
+    second_line = models.CharField(max_length=255, blank=True)
+    city = models.CharField(max_length=255)
+    # make this a ChoiceField?
+    state = models.CharField(max_length=2)
+    postal_code = models.CharField(max_length=10) # ZIP: 5 digits + dash + 4 digits
