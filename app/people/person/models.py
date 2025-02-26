81740ebb46c951f8384f8e2296311e20c611e352
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-02-26 16:30:15 -0500

    Add fields to Person, create table PersonAddress.

--- app/people/person/models.py
+++ app/people/person/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.db.models.functions import Now
 
 class Person(models.Model):
     name = models.CharField(max_length=255)
+    nickname = models.CharField(max_length=255, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    first_contact = models.DateField(db_default=Now(), blank=True)
+
+
+class PersonAddress(models.Model):
+    label = models.CharField(max_length=255, default="Home")
+    person = models.ForeignKey("person.person", on_delete=models.CASCADE)
+    address = models.ForeignKey("address.address", on_delete=models.CASCADE)
