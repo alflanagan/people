ea915c74627cb4e33062cbd35792dcbafff65436
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-03-01 13:14:56 -0500

    The config file for emacs djangonaut.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil
+  (python-shell-process-environment . ("DJANGO_SETTINGS_MODULE=people.settings"))
+  (python-shell-extra-pythonpaths . ("/Users/adrianflanagan/Devel/personal/people/app/people/"))
+  (python-shell-virtualenv-root . "/Users/adrianflanagan/Devel/personal/people/app/.venv/")))
