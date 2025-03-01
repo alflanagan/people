00389a964a9e9aee557bb41289fa1466f14c7d87
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-03-01 13:29:25 -0500

    Basic admin configurations.

--- app/people/organization/admin.py
+++ app/people/organization/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Organization, OrganizationPerson
+
+class OrganizationPersonInline(admin.TabularInline):
+        model = OrganizationPerson
+
+@admin.register(Organization)
+class OrganizationAdmin(admin.ModelAdmin):
+    inlines = [OrganizationPersonInline]
--- app/people/person/admin.py
+++ app/people/person/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Person, PersonAddress, PersonPhone
 
-# Register your models here.
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    plural_name = "People"
+    list_select_related = True
+    search_fields = ["name", "nickname"]
+
+@admin.register(PersonAddress)
+class PersonAddressAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(PersonPhone)
