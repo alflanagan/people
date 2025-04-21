aacaae7cb70e1d2afee99a7832efcda6d142442c
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-04-21 16:32:02 -0400

    Remove Django server files. ng new --standalone --package-manager yarn --routing --skip-git --defaults people_web

--- app/people_web/src/app/app.component.spec.ts
+++ app/people_web/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'people' title`, () => {
+  it(`should have the 'people_web' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('people');
+    expect(app.title).toEqual('people_web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, people');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, people_web');
   });
 });
