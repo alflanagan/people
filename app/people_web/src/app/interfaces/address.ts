a1daff1c0d158352f545bbd3392a9e19e8faf696
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-04-23 17:03:13 -0400

    Remove unneeded fields, move shared to BaseAddress.

--- app/people_web/src/app/interfaces/address.ts
+++ app/people_web/src/app/interfaces/address.ts
@@ -1,15 +1,12 @@
 export interface BaseAddress {
   type: 'usa' | 'international';
-  name: string;
-  email?: string;
-  phone?: string;
+  street1: string;
+  street2?: string;
+  city: string;
 }
 
 export interface USAAddress extends BaseAddress {
   type: 'usa';
-  street1: string;
-  street2?: string;
-  city: string;
   state: string;
   zipCode: string;
   county?: string;
@@ -17,9 +14,6 @@ export interface USAAddress extends BaseAddress {
 
 export interface InternationalAddress extends BaseAddress {
   type: 'international';
-  street1: string;
-  street2?: string;
-  city: string;
   region?: string;
   postalCode?: string;
   country: string;
