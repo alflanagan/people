c41c6e8916920e1ad643892e8b30c5c33e97b914
A. Lloyd Flanagan <lloyd.flanagan@proton.me>
2025-04-24 09:54:51 -0400

    Moved file urls.py to correct location. Merged new settings into people/settings.py.

--- app/people/people/settings.py
+++ app/people/people/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
 
     "rest_framework",
+    "django_filters",
 
     "person",
     "address",
@@ -94,6 +95,14 @@ DATABASES = {
     }
 }
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
